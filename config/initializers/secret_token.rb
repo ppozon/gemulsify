079e8c42fc2871e68c9883b7cd712000adf7f24b
Patty Pozon <ppozon@yahoo.com>
2014-02-08 17:32:06 -0800

    Initialized my first git repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gemulsify::Application.config.secret_key_base = '448f8ece1105a0b809a53c2edc8c9583d9f72abedee1e1d95d12ddb6c02df84c46079863693f4ed7e44003c36b52580c8d215fbe205ed09dfaac07eddec9138d'
